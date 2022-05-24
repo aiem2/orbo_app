3d88b2648edc606cd51ffe4edc4de0b065534bbc
Ignacio Espinosa <emprendiendoem@gmail.com>
2022-05-24 10:43:12 -0400

    Se agregaron todos los modelos a Active Admin

---
+++ app/admin/prospects.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Prospect do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :email, :area, :liason_name, :liason_phone, :liason_mail, :logo_url, :web, :fb, :ig, :description, :address, :city, :state, :phone
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :email, :area, :liason_name, :liason_phone, :liason_mail, :logo_url, :web, :fb, :ig, :description, :address, :city, :state, :phone]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
